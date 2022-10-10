7b7fda7b5c20a0db4fb6e49ac0c6471451c5be25
salamandermartin <salamancam.programming@gmail.com>
2022-10-10 17:52:02 -0400

    textfield entry

--- src/components/QuestionEntryModal.js
+++ src/components/QuestionEntryModal.js
@@ -1,10 +1,26 @@
 import './component_styles/questionEntry.css'
 import { TextField } from '@mui/material'
+import Box from '@mui/material/Box';
+import {useState, useEffect} from 'react'
+
 
 
 const QuestionEntryModal = (props) => {
+
+  const [messageQ, setMessageQ] = useState('dsadasds')
+  const [messageA, setMessageA] = useState('')
+
   const hideEntryModal = () => {
     props.setShowModal(false)
+  }
+
+  function handleChangeQ(e) {
+    setMessageQ(e.target.value)
+    console.log('hi')
+  }
+  function handleChangeA(e) {
+    setMessageA(e.target.value)
+    console.log(e.target.value)
   }
 
   return (
@@ -14,19 +30,24 @@ const QuestionEntryModal = (props) => {
             <h2 className = "modal-title">Entry for {props.score} point question here</h2>
         </div>
 
-        {/* <div className = "text-entry">
-          <textarea rows = "4">
-            Enter Question Here
-          </textarea>
-
-          <textarea rows = "4">
-            Enter Answer Here
-          </textarea>
-        </div> */}
-
         <div className = "textEntry">
-          <span className = "entry"><TextField multiline className = "questionEntryBox" minRows={5}/> </span>
-          <span><TextField multiline className = "answerEntryBox" minRows={5} /> </span>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          autoComplete="off"
+        >
+
+          <span className = "entry">
+            <TextField multiline className = "questionEntryBox" minRows={5} placeholder = "Enter your question here"
+            value = {messageQ} onChange={e => setMessageQ(e.target.value)}  />
+           </span>
+          <span>
+            <TextField multiline className = "answerEntryBox" minRows={5} placeholder = "Enter your answer here" 
+             onChange={handleChangeA} value = {messageA} /> 
+          </span>
+        </Box>
         </div>
 
         <div className = "entryModalClose" onClick = {() => hideEntryModal()}>
